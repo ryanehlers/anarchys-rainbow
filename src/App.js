48b5214db9422a105be15f901f8bf97f13b407de
Ryan Ehlers <ryan.ehlers@oup.com>
2020-07-03 13:08:48 +0100

    Can get URL query string

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Button from './Button.js'
+import Button from './Button.js';
 import logo from './logo.svg';
+import getParameterByName from './getParameterByName.js';
 import './App.css';
 
 function App() {
+  console.log(getParameterByName("x"));
   return (
     <div className="App">
       <div className="Header">
