ad8087fa65565d4eaf56d2ab488ab547a41a8580
Ryan Ehlers <ryan.ehlers@oup.com>
2020-07-03 18:22:20 +0100

    Buttons click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header.js';
 import Home from './Home.js';
 import getParameterByName from './getParameterByName.js';
 import './App.css';
 
-function App() {
-  console.log(getParameterByName("x"));
-  return (
-    <div className="App">
-      <Header className="Header" />
-      <Home />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.press = this.press.bind(this);
+  }
+
+  press(button) {
+    console.log('click');
+    //updateState(this, button);
+  }
+
+  render () {
+    getParameterByName('x');
+    return (
+      <div className="App">
+        <Header className="Header" press={this.press} />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Button from './Button.js';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.press = this.press.bind(this);
+  }
+
+  press(name) {
+    this.props.press(name);
+  }
+
   render() {
     return (
       <header>
         <div className="HeaderButtons">
-          <Button name="Music" />
-          <Button name="Blog" />
+          <Button name="Music" press={this.press} />
+          <Button name="Blog" press={this.press} />
         </div>
         <hr />
       </header>
